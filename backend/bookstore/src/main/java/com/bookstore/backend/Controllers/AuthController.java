8530332be576015ea5b03ddc16a249a54abc6b40
Dvareh <cobalt574@gmail.com>
2026-04-02 02:16:26 +0200

    fix(Controllers): allowed request from frontend port to AuthController

--- backend/bookstore/src/main/java/com/bookstore/backend/Controllers/AuthController.java
+++ backend/bookstore/src/main/java/com/bookstore/backend/Controllers/AuthController.java
@@ -8,12 +8,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/auth")
 @Tag(name = "Authentication Controller", description = "Controller for registration and login")
